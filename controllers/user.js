518ae41109871febd2e6c05385e10e29c429ddae
coderXgourav <softwaredevelopergourav@gmail.com>
2024-05-21 19:24:16 +0530

    gourav

--- controllers/user.js
+++ controllers/user.js
@@ -14,14 +14,24 @@ const returnToUser = (status,title,msg,icon) =>{
 
 // userSignup function 
 const userSignup = async (req, res) =>{
-   const {firstName,lastName,email,phoneNumber,password} = req.body;
+   const {firstName,lastName,email,phoneNumber,password,confirmPassword} = req.body;
 
-   if(!firstName || !lastName || !email || !phoneNumber || !password){
+   if(!firstName || !lastName || !email || !phoneNumber || !password || !confirmPassword){
       const response = returnToUser(false,"Error","Please fill all the input fields","error");
       return res.json(response);
    }else{
    const emailExist = await userModel.countDocuments({email:email});
    const numberExist = await userModel.countDocuments({phone_number:phoneNumber});
+
+   if(phoneNumber.length<10 || phoneNumber.length>10){
+    const response = returnToUser(false,"Error","Please enter a valid phone number.","error");
+    return res.json(response);
+   }
+
+   if(password!=confirmPassword){
+    const response = returnToUser(false,"Error","Mismatch between password and confirm password.","error");
+    return res.json(response);
+   }
 
    if(emailExist>0){
     const response = returnToUser(false,"Error","Looks like you've already signed up with that email","error");
@@ -55,27 +65,28 @@ const userLogin = async (req,res)=>{
     const {username , password}= req.body;
    if(!username || !password){
     const response = returnToUser(false,"Error","Fill email and password fields","error");
-    res.json(response);
+    return res.json(response);
    }
    try{
     const checkEmail = await userModel.findOne({email:username});
     if(checkEmail){
         if(checkEmail.password == password){
            const token = jwt.sign({name:checkEmail.first_name,id:checkEmail._id},SECRET_KEY);
+           res.setHeader('token',token);
            res.cookie('user',token);
            const response = returnToUser(true,"Success","Congratulations, you're logged in!.","success");
-           res.json(token);
+          return res.json(response);
         }else{
             const response = returnToUser(false,"Error","Incorrect password. Please try again.","error");
-            res.json(response);
+            return res.json(response);
         }
     }else{
         const response = returnToUser(false,"Error","Invalid email or password. Please try again.","error");
-          res.json(response);
+        return res.json(response);
     }
    }catch (err){
     const response = returnToUser(false,"Error","Server crashed","error");
-    res.json(response);
+    return res.json(response);
    }
   
    
